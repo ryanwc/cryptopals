37e79815b764c4ba3a119ad36acaf82ce0ed1a4d
Patrick Nance <jpnance@gmail.com>
2020-01-20 08:06:49 -0800

    Test-driven development is so cool at this level.

--- jpnance/set1/set1.py
+++ jpnance/set1/set1.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 
-print("Hello, world!")
+import unittest
+
+class TestMethods(unittest.TestCase):
+
+	def testHexToBase64(self):
+		self.assertEqual(hexToBase64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'), 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t')
+
+def hexToBase64(hex):
+	return 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+if __name__ == '__main__':
+	unittest.main()
