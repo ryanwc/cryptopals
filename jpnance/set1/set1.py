40844609f68b046c004622d726242a17acff8a3e
Patrick Nance <jpnance@gmail.com>
2020-01-20 09:35:22 -0800

    Okay, now this works for cases where the length of the hex string is a multiple of three. Good enough.

--- jpnance/set1/set1.py
+++ jpnance/set1/set1.py
@@ -7,7 +7,17 @@ class TestMethods(unittest.TestCase):
 		self.assertEqual(hexToBase64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'), 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t')
 
 def hexToBase64(hex):
-	return 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+	conversionString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+	outputString = ''
+
+	for i in range(int(len(hex) / 3)):
+		offset = i * 3
+
+		outputString += conversionString[(int(hex[offset + 0], 16) << 2) | (int(hex[offset + 1], 16) >> 2)]
+		outputString += conversionString[((int(hex[offset + 1], 16) & 0x3) << 4) | (int(hex[offset + 2], 16))]
+
+
+	return outputString
 
 if __name__ == '__main__':
 	unittest.main()
