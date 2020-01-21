00191d6ab9a24a770f11d5c5ea2b0b7f8bac3bce
Kevin Connor <kevinpaulconnor@gmail.com>
2020-01-20 20:03:11 -0600

    A 1.1 solution. Something about this feels wrong? Specifically, the String variable declaration, and then to_string()-ing the string literal, which I guess is just a str literal? Strings in Rust are weird.

--- kevinpaulconnor/src/main.rs
+++ kevinpaulconnor/src/main.rs
@@ -1,3 +1,5 @@
+mod set1;
+
 fn main() {
-    println!("Hello, world!");
+
 }
---
+++ kevinpaulconnor/src/set1.rs
@@ -0,0 +1,20 @@
+use hex;
+use base64;
+
+fn hex_to_base64(hex_string: String) -> String {
+    let decoded = hex::decode(hex_string);
+    let encoded = base64::encode(&decoded.unwrap());
+    return encoded;
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_hex() {
+        assert_eq!(hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string()), 
+            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+    }
+}
