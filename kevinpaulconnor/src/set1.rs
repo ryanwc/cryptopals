97c32967ee852b9d02b6a0556eca5fcb90371c53
Kevin Connor <kevinpaulconnor@gmail.com>
2020-01-30 21:11:27 -0600

    somewhat awkward 1.3 solution

--- kevinpaulconnor/src/set1.rs
+++ kevinpaulconnor/src/set1.rs
@@ -1,5 +1,6 @@
 use hex;
 use base64;
+use std::collections::HashMap;
 
  //1.1
 fn hex_to_base64(hex_string: String) -> String {
@@ -11,20 +12,110 @@ fn hex_to_base64(hex_string: String) -> String {
 fn fixed_xor(right_side: String, left_side: String) -> String {
     let decoded_right = hex::decode(right_side).unwrap();
     let decoded_left = hex::decode(left_side).unwrap();
-    let mut result: Vec<u8> = Vec::new();
-    for i in 0..decoded_left.len() {
-        result.push(decoded_right[i] ^ decoded_left[i]);
-    }
+    let result = fixed_xor_bytes(&decoded_right, &decoded_left);
     return hex::encode(&result);
 }
 
+fn fixed_xor_bytes(right: &Vec<u8>, left: &Vec<u8>) -> Vec<u8> {
+    let mut result: Vec<u8> = Vec::new();
+    for i in 0..left.len() {
+        result.push(right[i] ^ left[i]);
+    }
+    return result
+}
+
+fn xor_vector_against_single_byte(byte_vector: &Vec<u8>, single: u8) -> Vec<u8> {
+    let mut result: Vec<u8> = Vec::new();
+    for i in 0..byte_vector.len() {
+        result.push(byte_vector[i] ^ single);
+    }
+    return result
+}
+
+fn score_sequence(bytes: &Vec<u8>, table: &HashMap<u8, i32>) -> i32 {
+    let mut result: i32 = 0;
+    for i in 0..bytes.len() {
+        let byte_value = table.get(&bytes[i]);
+        result = result + byte_value.unwrap_or(&0);
+    }
+    return result;
+}
+
+//1.3
 pub fn solve_single_byte_xor_cipher() {
     let message = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
     let unhexed_message = hex::decode(message).unwrap();
     let mut test_byte: u8 = 0;
-    for i in 0..u8::max_value(){
-        println!("{}", test_byte);
-        test_byte = test_byte + 1;
+    println!("{:?}", unhexed_message);
+    // seeing if etaoin shrdlu *only* might be a shortcut
+    // narrator: it was not. I didn't get the right answer until I
+    // included spaces
+    let mut score_table: HashMap<u8, i32> = HashMap::new();
+    // there has got to be a less stupid way to do this
+    score_table.insert(b' ', 13);
+    score_table.insert(b'e', 12);
+    score_table.insert(b'E', 12);
+    score_table.insert(b't', 9);
+    score_table.insert(b'T', 9);
+    score_table.insert(b'a', 8);
+    score_table.insert(b'A', 8);
+    score_table.insert(b'o', 8);
+    score_table.insert(b'O', 8);
+    score_table.insert(b'i', 7);
+    score_table.insert(b'I', 7);
+    score_table.insert(b'n', 7);
+    score_table.insert(b'N', 7);
+    score_table.insert(b's', 6);
+    score_table.insert(b'S', 6);
+    score_table.insert(b'r', 6);
+    score_table.insert(b'R', 6);
+    score_table.insert(b'h', 6);
+    score_table.insert(b'H', 6);
+    score_table.insert(b'D', 4);
+    score_table.insert(b'd', 4);
+    score_table.insert(b'l', 4);
+    score_table.insert(b'L', 4);
+    score_table.insert(b'u', 3);
+    score_table.insert(b'U', 3);
+    score_table.insert(b'p', 2);
+    score_table.insert(b'P', 2);
+    score_table.insert(b'b', 1);
+    score_table.insert(b'B', 1);
+    score_table.insert(b'v', 1);
+    score_table.insert(b'V', 1);
+    score_table.insert(b'k', 1);
+    score_table.insert(b'K', 1);
+    score_table.insert(b'x', 1);
+    score_table.insert(b'X', 1);
+    score_table.insert(b'q', 1);
+    score_table.insert(b'Q', 1);
+
+    struct MessageScore {
+        message: Vec<u8>,
+        score: i32
+    }
+    let mut best_message = MessageScore {
+        message: Vec::new(),
+        score: 0
+    };
+    for i in 0..256 {
+        //println!("trying byte {}...", i);
+        let xor_message = xor_vector_against_single_byte(&unhexed_message, test_byte);
+        let score = score_sequence(&xor_message, &score_table);
+        //println!("score: {}, xor_message: {:?}", score, xor_message);
+        if score > best_message.score {
+            best_message.score = score;
+            best_message.message = xor_message;
+            let maybe_string = String::from_utf8(best_message.message);
+            if maybe_string.is_err() {
+                println!("new best score of {} but the message wasn't printable", score)
+            } else {
+                println!("new best score:{}, message: {}", score, maybe_string.unwrap())
+            }
+        }
+        if i < 255 {   
+            test_byte = test_byte + 1;
+        }    
     }
     //println!("{}", unhexed_message.len());
 }
