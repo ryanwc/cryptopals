9c14e4ff760cfe7e101aff5dc1ee7c05ef3a59c9
Kevin Connor <kevinpaulconnor@gmail.com>
2020-01-30 21:25:46 -0600

    run 1_3 with a command line argument

--- kevinpaulconnor/src/main.rs
+++ kevinpaulconnor/src/main.rs
@@ -1,5 +1,12 @@
+use std::env;
 mod set1;
 
 fn main() {
-    set1::solve_single_byte_xor_cipher();
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        let challenge = &args[1];
+        if challenge == "1_3" {
+            set1::solve_1_3();
+        }
+    }
 }
