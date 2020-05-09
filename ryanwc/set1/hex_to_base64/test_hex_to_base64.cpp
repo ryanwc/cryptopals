7bd14eeccd7cf0c25da966c1f6ee7e870cbcdc6d
ryanwc <ryanwc13@gmail.com>
2020-05-09 15:26:45 -0400

    tests for conver hex to bitstring

--- ryanwc/set1/hex_to_base64/test_hex_to_base64.cpp
+++ ryanwc/set1/hex_to_base64/test_hex_to_base64.cpp
@@ -9,6 +9,14 @@ namespace {
 
     TEST(ConvertHexCharToBitStringTest, IsValidHexCharSucceeds) {
         ASSERT_STREQ(std::string("0000").c_str(), convertHexCharToBitString('0').c_str());
+        ASSERT_STREQ(std::string("1111").c_str(), convertHexCharToBitString('F').c_str());
+        ASSERT_STREQ(std::string("1010").c_str(), convertHexCharToBitString('A').c_str());
+    }
+
+    TEST(ConvertHexCharToBitStringTest, InvalidHexCharThrows) {
+        ASSERT_THROW(convertHexCharToBitString('Z'), std::invalid_argument);
+        ASSERT_THROW(convertHexCharToBitString('a'), std::invalid_argument);
+        ASSERT_THROW(convertHexCharToBitString('&'), std::invalid_argument);
     }
 
 }  // namespace
