6a7eb2de07cc76b770ca0df70cc020961667e527
ryanwc <ryanwc13@gmail.com>
2020-05-10 11:26:17 -0400

    decide dont care about helper func unit tests; add throws tests to convert hex to base 64

--- ryanwc/set1/hex_to_base64/test_hex_to_base64.cpp
+++ ryanwc/set1/hex_to_base64/test_hex_to_base64.cpp
@@ -6,58 +6,8 @@
 
 
 namespace {
-/*
-    TEST(ConvertHexCharToBitString_Test, IsValidHexChar_succeeds) {
-        ASSERT_STREQ(std::string("0000").c_str(), convertHexCharToBitString('0').c_str());
-        ASSERT_STREQ(std::string("1111").c_str(), convertHexCharToBitString('F').c_str());
-        ASSERT_STREQ(std::string("1111").c_str(), convertHexCharToBitString('f').c_str());
-        ASSERT_STREQ(std::string("1010").c_str(), convertHexCharToBitString('A').c_str());
-        ASSERT_STREQ(std::string("1010").c_str(), convertHexCharToBitString('a').c_str());
-    }
 
-    TEST(ConvertHexCharToBitString_Test, InvalidHexChar_throws) {
-        ASSERT_THROW(convertHexCharToBitString('Z'), std::invalid_argument);
-        ASSERT_THROW(convertHexCharToBitString('&'), std::invalid_argument);
-    }
-
-    TEST(ConvertSixBitStringToBase64Char_Test, CapitalLetters_succeeds) {
-        ASSERT_EQ('A', convertSixBitStringToBase64Char("000000"));
-        ASSERT_EQ('R', convertSixBitStringToBase64Char("010001"));
-        ASSERT_EQ('Z', convertSixBitStringToBase64Char("011001"));
-    }
-
-    TEST(ConvertSixBitStringToBase64Char_Test, LowerCaseLetters_succeeds) {
-        ASSERT_EQ('a', convertSixBitStringToBase64Char("011010"));
-        ASSERT_EQ('n', convertSixBitStringToBase64Char("100111"));
-        ASSERT_EQ('z', convertSixBitStringToBase64Char("110011"));
-    }
-
-    TEST(ConvertSixBitStringToBase64Char_Test, Numbers_succeeds) {
-        ASSERT_EQ('0', convertSixBitStringToBase64Char("110100"));
-        ASSERT_EQ('4', convertSixBitStringToBase64Char("111000"));
-        ASSERT_EQ('9', convertSixBitStringToBase64Char("111101"));
-    }
-
-    TEST(ConvertSixBitStringToBase64Char_Test, PlusSign_succeeds) {
-        ASSERT_EQ('+', convertSixBitStringToBase64Char("111110"));
-    }
-
-    TEST(ConvertSixBitStringToBase64Char_Test, ForwardSlash_succeeds) {
-        ASSERT_EQ('/', convertSixBitStringToBase64Char("111111"));
-    }
-
-    TEST(ConvertSixBitStringToBase64Char_Test, InvalidStringLength_throws) {
-        ASSERT_THROW(convertSixBitStringToBase64Char("1111111"), std::invalid_argument);
-        ASSERT_THROW(convertSixBitStringToBase64Char("11111"), std::invalid_argument);
-    }
-
-    TEST(ConvertSixBitStringToBase64Char_Test, NotBitString_throws) {
-        ASSERT_THROW(convertSixBitStringToBase64Char("hel|O1"), std::invalid_argument);
-        ASSERT_THROW(convertSixBitStringToBase64Char("101200"), std::invalid_argument);
-    }
-
-    */
-    TEST(ConvertHexxStringToBase64_Test, ShortString_succeeds) {
+    TEST(ConvertHexStringToBase64_Test, ShortString_succeeds) {
         ASSERT_STREQ(std::string("TQ==").c_str(), convertHexStringToBase64("4D").c_str());
         ASSERT_STREQ(std::string("lj0=").c_str(), convertHexStringToBase64("963d").c_str());
         ASSERT_STREQ(std::string("IrQ=").c_str(), convertHexStringToBase64("22b4").c_str());
@@ -65,7 +15,7 @@ namespace {
         ASSERT_STREQ(std::string("SKvW").c_str(), convertHexStringToBase64("48abd6").c_str());
     }
 
-    TEST(ConvertHexxStringToBase64_Test, LongString_succeeds) {
+    TEST(ConvertHexStringToBase64_Test, LongString_succeeds) {
         ASSERT_STREQ(
             std::string("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t").c_str(), 
             convertHexStringToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d").c_str()
@@ -90,6 +40,18 @@ namespace {
             std::string("4wWFygJmRhovsbG1d+F811Ewr2D8PLmSZg==").c_str(), 
             convertHexStringToBase64("e30585ca0266461a2fb1b1b577e17cd75130af60fc3cb99266").c_str()
         );
+    }
+
+    TEST(ConvertHexStringToBase64_Test, InvalidHexChar_throws) {
+        ASSERT_THROW(convertHexStringToBase64("4Z"), std::invalid_argument);
+        ASSERT_THROW(convertHexStringToBase64("4&"), std::invalid_argument);
+        ASSERT_THROW(convertHexStringToBase64("e30585ca0266461a2f*1b1b577e17cd75130af60fc3cb99266"), std::invalid_argument);
+    }
+
+    TEST(ConvertHexStringToBase64_Test, InvalidHexLength_throws) {
+        ASSERT_THROW(convertHexStringToBase64("4"), std::invalid_argument);
+        ASSERT_THROW(convertHexStringToBase64("4D0"), std::invalid_argument);
+        ASSERT_THROW(convertHexStringToBase64("e30585ca0266461a2fb1b1b577e17cd75130af60fc3cb992660"), std::invalid_argument);
     }
 
 }  // namespace
