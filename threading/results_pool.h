771174e43c71d1c4d1bc0a929b111fd4184ff51e
ryanwc <ryanwc13@gmail.com>
2020-07-25 19:42:35 -0400

    use partial template specialization to generalize ResultsPool to any non-member results func

--- threading/results_pool.h
+++ threading/results_pool.h
@@ -1,23 +1,52 @@
 #ifndef THREADING_POOL_H__
 #define THREADING_POOL_H__
 
+#include <tuple>
 #include <thread>
 #include <queue>
 #include <mutex>
 #include <condition_variable>
 
 
+
 namespace CustomThreading
 {
+
+    // use partial template specialization to "unwrap" a function signature (e.g. bool(int, int))
+    template<typename ResultsFuncSig>
+    class ResultsPool {};
+
     /*
      * Pool for distributing work to worker threads, then retrieiving
      * results from those worker threads after all work is completed.
      * Abstracts away threading details (e.g. syncronization on work queue), 
      * client just needs to use public API.
+     * Templated by the return type and arg types of the results func passed as a poniter to the
+     * constructor. Results func is the func that will be executed by pool workers on work submission.
+     * Must use std::function-style templating, e.g. to use a results function
+     * that takes an int, a bool, and returns a float, would template like
+     * ResultsPool<float(int, bool)>. So, can use with decltype() since that is what is returned
+     * for functions.
+     * Caveat 1: using decltype(myFunc) will not compile if myFunc is overloaded.
+     *   but can still provide the signature without using decltype.
+     * Caveat 2: does not work with member functions.
+     * Example:
+     *   float foo(int one, bool two) { return 2.0; };
+     *   ResultsPool<decltype(foo)> fooResultsPool(foo);
+     *   // alternately: ResultsPool<float(int,bool)> fooResultsPool(foo);
+     *   fooResultsPool.SubmitWork(1, true);
+     *   fooResultsPool.SubmitWOrk(2, false);
+     *   auto fooResults = fooResultsPool.WaitForAllResults();  // len 2 vector of float, each entry == 2.0
      */
-    template<typename ResultType, typename... ResultsFuncArgs>
-	class ResultsPool {
+    template<typename RetT, typename ...ArgTs>
+	class ResultsPool<RetT(ArgTs...)> {
 		public:
+
+            // just a convenient name for func pointer
+            typedef RetT(*PoolResultsFuncPtr)(ArgTs...);
+
+            // the func used to produce results by workers in this pool
+            PoolResultsFuncPtr ResultsFuncPtr;
 
             /* 
              * Enumerate possible states of a ResultPool
@@ -30,9 +59,11 @@ namespace CustomThreading
                 STOPPING  // workers are being deactivated.
             };
 
-			typedef void ResultsFunc(ResultsFuncArgs...);
-
-			ResultsPool(int numThreads);
+            /*
+             * Construct a new ResultsPool with numThreads workers executing func pointed to by resultsFuncPtr
+             * when work is subitted.
+             */
+			ResultsPool(int numThreads, PoolResultsFuncPtr resultsFuncPtr);
 			~ResultsPool();
 
             /*
@@ -54,37 +85,46 @@ namespace CustomThreading
             /*
              * Submit some work to the pool. It will be picked up by any available workers.
              */
-            void SubmitWork(ResultsFunc*);
+            void SubmitWork(ArgTs...);  // TODO: bind the args to the func provided in constructor
 
             /*
              * Blocks the caller until all previously submitted work has been completed, 
              * then returns a list of the results.
              */
-            std::vector<ResultType> WaitForAllResults();
+            std::vector<RetT> WaitForAllResults();
 
             /*
              * Retrieves all results from already-completed work.
              * The pool will 'forget' the retrieved results, so the caller should save them if desired.
              */
-            std::vector<ResultType> GetAvailableResults();
+            std::vector<RetT> GetAvailableResults();
 
 		private:
 
-			static void _endWorker(void* data);
+            // kill a worker currently alive in the pool
+			static void _endWorker();
+            // start all workers in the pool
             void _startWorkers();
+            // the entry point for pool worker threadss
             void _workerLoop(int threadNum);
 
+            // status of this pool
             Status _status;
+            // number of max worker threads in this pool
 			int _numThreads;
-            // TODO: cache alignment helpful for any of these?
+            // TODO: cache alignment helpful for any of these? probably _workerResults?
+            // keep track of the workers in the pool
 			std::unique_ptr<std::thread[]> _workers;
-            std::unique_ptr<std::vector<ResultType>> _workerResults;
+            // each worker has its own results list so do not have to syncronize so often
+            std::unique_ptr<std::vector<RetT>> _workerResults;
+            // need a mutex so master thread can safely access each worker result list
             std::unique_ptr<std::mutex> _workerResultsMutexes;
-            std::queue<std::tuple<ResultsFunc*, void*>> _workQueue;
-            std::vector<ResultType> _results;
+            // the queue of work workers pull work from
+            std::queue<std::tuple<ArgTs...>> _workQueue;
+            // the signal from master to workers that work has been added to the queue
 			std::condition_variable _addedWorkCondition;
+            // need to sync on the work queue
 			std::mutex _workQueueMutex;
-            std::mutex _resultsMutex;
 	};
 }
 
