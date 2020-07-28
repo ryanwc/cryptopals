f1a8f663314269fe1a9bf578543782e103f4d990
ryanwc <ryanwc13@gmail.com>
2020-07-27 22:53:41 -0400

    some setup for the app

--- web_app/app/app.py
+++ web_app/app/app.py
@@ -1,4 +1,5 @@
 from typing import Tuple
+from http import HTTPStatus
 
 from flask import Flask
 
@@ -13,9 +14,29 @@ def healthcheck() -> Tuple[str, int]:
     return 'OK', 200
 
 
-@APP.route('/hello')
-def say_hello():
-    return 'Hello', 200
+@APP.route('/translate')
+def translate():
+    """
+    Translate a string representation of some bits to another kind of string representation
+    (e.g. hex to base64).
+    """
+    return 'not implemented', HTTPStatus.NOT_IMPLEMENTED
+
+
+@APP.route('/bitOperate')
+def bit_operate():
+    """
+    Perform a bitwise operator on the given bits (e.g. XORing two hex strings and returning a hex string).
+    """
+    return 'not implemented', HTTPStatus.NOT_IMPLEMENTED
+
+
+@APP.route('/decode')
+def decode():
+    """
+    Decode the given string.
+    """
+    return 'not implemented', HTTPStatus.NOT_IMPLEMENTED
 
 
 if __name__ == '__main__':
